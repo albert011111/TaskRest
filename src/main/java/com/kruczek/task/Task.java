09cc8ccbf989f83ee15ec81259f13ca4919ce38b
Patryk Kruczek <patryk.kruczek@gmail.com>
2018-11-20 20:46:38 +0100

    Pierwszy commit

---
+++ src/main/java/com/kruczek/task/Task.java
@@ -0,0 +1,66 @@
+package com.kruczek.task;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+
+@Entity(name = "tasks")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createDate", "executeDate"}, allowGetters = true)
+public class Task {
+    @Id
+    @GeneratedValue
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "CREATE_DATE", nullable = false, updatable = false)
+    @CreatedDate
+    @Temporal(value = TemporalType.DATE)
+    private Date createDate;
+
+    @Column(name = "EXECUTE_DATE", nullable = false)
+    @LastModifiedDate
+    @Temporal(value = TemporalType.DATE)
+    private Date executeDate;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getExecuteDate() {
+        return executeDate;
+    }
+
+    public void setExecuteDate(Date executeDate) {
+        this.executeDate = executeDate;
+    }
+}
---
+++ src/main/java/com/kruczek/task/TaskController.java
@@ -0,0 +1,50 @@
+package com.kruczek.task;
+
+
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+//@CrossOrigin(origins = "http://localhost:4201", maxAge = 3600)
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+
+    private static final String TASKS = "/tasks";
+    private static final String TASKS_TASK_ID = TASKS + "/{taskId}";
+
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping(TASKS)
+    public List<Task> getTasks() {
+        return this.taskRepository.findAll();
+    }
+
+    @GetMapping(TASKS_TASK_ID)
+    public Optional<Task> getTaskById(@PathVariable Long taskId) {
+        return this.taskRepository.findById(taskId);
+    }
+
+    @PutMapping(TASKS)
+    public Task addTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping(TASKS_TASK_ID)
+    public ResponseEntity<?> deleteTaskById(@PathVariable Long taskId) {
+        return taskRepository.findById(taskId)
+                .map(task -> {
+                    taskRepository.delete(task);
+                    return ResponseEntity.status(HttpStatus.OK).build();
+                }).orElseThrow(() -> new ResourceNotFoundException("Task with id: " + taskId + " not found!"));
+    }
+}
