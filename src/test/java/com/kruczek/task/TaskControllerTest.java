4bfc9d7775abd9314018d17cdeb3dec903bb79ce
Patryk Kruczek <patryk.kruczek@gmail.com>
2018-11-20 20:46:38 +0100

    Pierwszy commit

--- src/test/java/com/kruczek/task/TaskControllerTest.java
+++ src/test/java/com/kruczek/task/TaskControllerTest.java
@@ -1,17 +1,4 @@
 package com.kruczek.task;
 
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {TestContext.class, TaskRepository.class, TaskController.class})
-@WebAppConfiguration
 public class TaskControllerTest {
-
-
 }
-
-
